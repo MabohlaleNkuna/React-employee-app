0db2ffa8e62d8277fbb8c34f5a14e7dac44cbd0b
MabohlaleNkuna <mabohlalenkuna@gmail.com>
2024-07-17 03:17:12 +0200

    initial

---
+++ src/components/HeaderComp.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const HeaderComp = () =>(
+    <div className="header">
+        <h1>Employee management App</h1>
+        <nav>
+             <a href="#home">Home</a>
+             <a href="#employees">Employees</a>
+             <a href="#add-employee">Add Employees</a>
+         </nav>
+    </div>
+);
+export default HeaderComp;
